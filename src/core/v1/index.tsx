8896fef6477c5d5ccf4194800047c58b98168128
yanxingzhe <yanxingzhe@pinduoduo.com>
2021-05-31 12:06:21 +0800

    fix: try stream

--- src/core/v1/index.tsx
+++ src/core/v1/index.tsx
@@ -1,26 +1,16 @@
 console.log("...inner");
+// import { Readable } from 'stream'
+import * as Stream from "stream";
 
-import * as React from "react";
-import { renderToString } from "react-dom/server";
-import { writeFile } from "../../util";
+var src = new Stream();
+src.readable = true;
 
-const Document = props => {
-  return (
-    <html>
-      <header>header</header>
-      <body>
-        <span>inner</span>
-        {props}
-      </body>
-    </html>
-  );
+var dest = new Stream();
+dest.writable = true;
+dest.write = function (data) {
+  console.log(data == "test");
 };
 
-const Hello = () => <div>hello</div>;
+src.pipe(dest);
 
-// 1、理解 renderToString
-const result = renderToString(React.createElement(Document, <Hello />));
-
-writeFile("static/index.html", result);
-
-console.log("...result", result);
+src.emit("data", "test");
