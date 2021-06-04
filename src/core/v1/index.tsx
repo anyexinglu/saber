fb5d32735b4a6768495ef31fd30cea87b9b12591
yanxingzhe <yanxingzhe@pinduoduo.com>
2021-06-04 10:22:11 +0800

    add views, 404, and router regex

--- src/core/v1/index.tsx
+++ src/core/v1/index.tsx
@@ -5,25 +5,40 @@ import Koa from "koa";
 // 和 cv 最主要的区别是，后者统一走 render 作为 reqHandler。
 import * as route from "koa-route";
 import errorHandler from "./errorHandler";
+import * as React from "react";
+import * as reactDom from "react-dom/server";
+
+const { renderToString } = reactDom;
 
 const app = new Koa();
 
-const about = ctx => {
-  // 抛出错误
-  ctx.throw(500);
-  ctx.response.type = "html";
-  ctx.response.body = '<a href="/">Index Page</a>';
-};
-
 const main = ctx => {
-  ctx.response.body = "Hello World";
+  // ctx.throw(500);
+  const { path } = ctx;
+  const regex = new RegExp(`^/([0-9a-zA-Z_-]+)`);
+  const match = path.match(regex);
+  console.log("...ctx", path, match);
+  const view = match?.[1];
+
+  let Page;
+  try {
+    Page = require(`./views/${view}`).default;
+  } catch (e) {
+    // Error: Cannot find module './views/12'
+    console.log("...e", e);
+    Page = require(`./views/404`).default;
+  }
+
+  console.log("...result", Page, React.createElement(Page));
+  ctx.response.body = renderToString(React.createElement(Page));
+  ctx.response.type = "html";
 };
 
-app.use(route.get("/", main));
+app.use(route.get("*", main));
+
 app.use(errorHandler);
 app.on("error", (err, ctx) => {
   console.error("server error", err, ctx);
 });
-app.use(route.get("/about", about));
 
 app.listen(3000);
