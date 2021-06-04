bf8f50bc4247301221325de318dcb01fe1d2b852
yanxingzhe <yanxingzhe@pinduoduo.com>
2021-06-04 11:10:10 +0800

    fix: 试着解决favicon.ico的报错，但后面自己没了

---
+++ src/core/v1/helper/favicon.ts
@@ -0,0 +1,49 @@
+/**
+ * Module dependencies.
+ */
+import { resolve } from "path";
+import fs from "fs";
+console.log("...resolve", resolve, fs);
+
+export default function (path, options?: any) {
+  console.log(11111);
+  if (!path) {
+    return (ctx, next) => {
+      if ("/favicon.ico" != ctx.path) {
+        return next();
+      }
+    };
+  }
+
+  path = resolve(path);
+  options = options || {};
+
+  let icon;
+  const maxAge =
+    options.maxAge == null
+      ? 86400000
+      : Math.min(Math.max(0, options.maxAge), 31556926000);
+  const cacheControl = `public, max-age=${(maxAge / 1000) | 0}`;
+  const mime = options.mime || "image/x-icon";
+
+  return (ctx, next) => {
+    console.log(222, ctx.path, "/favicon.ico" != ctx.path);
+    if ("/favicon.ico" != ctx.path) {
+      return next();
+    }
+
+    console.log("........in favicon");
+
+    if ("GET" !== ctx.method && "HEAD" !== ctx.method) {
+      ctx.status = "OPTIONS" == ctx.method ? 200 : 405;
+      ctx.set("Allow", "GET, HEAD, OPTIONS");
+    } else {
+      // lazily read the icon
+      if (!icon) icon = fs.readFileSync(path);
+      ctx.set("Cache-Control", cacheControl);
+      ctx.type = mime;
+      console.log("....icon", icon);
+      ctx.body = icon;
+    }
+  };
+}
--- src/core/v1/index.tsx
+++ src/core/v1/index.tsx
@@ -4,20 +4,28 @@ import Koa from "koa";
 // 很简单，就是正则命中 path：https://github.com/koajs/route/blob/master/index.js
 // 和 cv 最主要的区别是，后者统一走 render 作为 reqHandler。
 import * as route from "koa-route";
+// import favicon from "./helper/favicon";
+// import * as path from "path";
 import errorHandler from "./errorHandler";
 import * as React from "react";
 import * as reactDom from "react-dom/server";
+// import * as serve from "koa-static";
 
 const { renderToString } = reactDom;
-
 const app = new Koa();
+// const root = path.join(__dirname);
 
 const main = ctx => {
   // ctx.throw(500);
   const { path } = ctx;
   const regex = new RegExp(`^/([0-9a-zA-Z_-]+)`);
+  console.log("...ctx", path);
+  // if (["/favicon.ico"].includes(path)) {
+  //   return serve(root);
+  // }
+
   const match = path.match(regex);
-  console.log("...ctx", path, match);
+  console.log("...match", path, match);
   const view = match?.[1];
 
   let Page;
@@ -33,6 +41,16 @@ const main = ctx => {
   ctx.response.body = renderToString(React.createElement(Page));
   ctx.response.type = "html";
 };
+
+// 奇怪自己就没有 favicon.ico 的报错了
+// const appRoot = path.resolve(
+//   path.dirname(require.resolve("koa")),
+//   "..",
+//   "..",
+//   ".."
+// );
+
+// app.use(favicon(appRoot + "/static/favicon.ico"));
 
 app.use(route.get("*", main));
 
