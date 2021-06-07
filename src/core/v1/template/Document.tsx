e637514d09b4fd03a19de66039939e4d69fc95b4
yanxingzhe <yanxingzhe@pinduoduo.com>
2021-06-07 10:17:43 +0800

    fix tag

--- src/core/v1/template/Document.tsx
+++ src/core/v1/template/Document.tsx
@@ -4,8 +4,8 @@ export default function Document({ children }) {
   return (
     <html class="no-js" lang="">
       <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <title>Saber</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
