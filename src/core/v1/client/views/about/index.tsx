9d787eddfa9f1034570b7f0636639993e9ef7a88
yanxingzhe <yanxingzhe@pinduoduo.com>
2021-06-30 11:42:40 +0800

    fix: add @types/koa

--- src/core/v1/client/views/about/index.tsx
+++ src/core/v1/client/views/about/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
-import { createHotContext as __vite__createHotContext } from "../../client";
+// import { createHotContext as __vite__createHotContext } from "../../client";
+
+// console.log("...__vite__createHotContext", __vite__createHotContext);
 
 // import.meta.hot =
-__vite__createHotContext("/about/index.tsx");
+// __vite__createHotContext("/about/index.tsx");
 
 export default function About() {
   console.log("....aboxxxutaxx");
